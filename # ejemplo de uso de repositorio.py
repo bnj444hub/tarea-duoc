d7ddbb680e9caf867b85a7adbb9b13045c0ad053
bnj444hub <134544456+bnj444hub@users.noreply.github.com>
2023-05-31 15:31:04 -0400

    Delete # ejemplo de uso de repositorio.py

--- # ejemplo de uso de repositorio.py
+++
@@ -1,9 +0,0 @@
-# ejemplo de uso de repositorio
-
-print("datos personales")
-print("-----------------------")
-vNom = input("ingrese su nombre : ")
-vEdad = input("ingrese su edad : ")
-print("-------------------------------")
-print(f"su nombre es : {vNom}")
-print(f"su edad es : {vEdad}")
